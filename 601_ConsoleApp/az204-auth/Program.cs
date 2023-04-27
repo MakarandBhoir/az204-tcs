ec2f821bb4b423a90f7f9a006bada1e1d00b3be5
makarandbhoir <makarand.bhoir@live.com>
2023-04-27 18:03:49 +0530

    security code

---
+++ 601_ConsoleApp/az204-auth/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace az204_auth
+{
+    class Program
+    {
+        private const string _clientId = "3ccdbb2d-152c-4019-a5e4-dd1597a43d76";
+        private const string _tenantId = "ae6cba77-f65c-49f3-b41d-37f9945b7135";
+
+        public static async Task Main(string[] args)
+        {
+            var app = PublicClientApplicationBuilder
+                .Create(_clientId)
+                .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+                .WithRedirectUri("http://localhost")
+                .Build();
+            string[] scopes = { "user.read" };
+            AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+
+            Console.WriteLine($"Token:\t{result.AccessToken}");
+        }
+    }
+}
